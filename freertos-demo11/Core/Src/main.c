8cdfc911c7b3904559f66c6d413625bfcb4265f5
lst <3235244743@qq.com>
2026-05-24 17:52:58 +0800

    事件组，与操作

---
+++ freertos-demo11/Core/Src/main.c
@@ -0,0 +1,297 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "cmsis_os.h"
+#include "usart.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "task.h"
+#include <stdio.h>
+#include "queue.h"
+#include "semphr.h"
+#include "event_groups.h"
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+//启动任务配置参数
+#define START_PRIO 1
+#define START_STACK_SIZE 128
+TaskHandle_t StartTask_Handler;
+void Start_Task(void *pvParameters);
+
+//设置事件组任务配置参数
+#define SET_PRIO 5
+#define SET_STACK_SIZE 128
+TaskHandle_t set_Handler;
+void Set_Task(void *pvParameters);
+
+//等待事件组任务配置参数
+#define WAIT_PRIO 4
+#define WAIT_STACK_SIZE 128
+TaskHandle_t wait_Handler;
+void Wait_Task(void *pvParameters);
+
+
+//事件组句柄
+EventGroupHandle_t eventGroup=NULL;
+//定义事件掩码
+#define KEY1_EVENT (0x01<<0)
+#define KEY2_EVENT (0x01<<1)
+
+//high task和mid task双双osDelay(即阻塞)时，low task才能执行
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+void MX_FREERTOS_Init(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_USART1_UART_Init();
+	
+  /* USER CODE BEGIN 2 */
+	
+	//创建启动任务
+   xTaskCreate(Start_Task,"Start_Task",START_STACK_SIZE,NULL,START_PRIO,&StartTask_Handler);
+   //启动调度器
+	 vTaskStartScheduler();
+  /* USER CODE END 2 */
+
+  /* Init scheduler */
+  osKernelInitialize();
+
+  /* Call init function for freertos objects (in cmsis_os2.c) */
+  MX_FREERTOS_Init();
+
+  /* Start scheduler */
+  osKernelStart();
+
+  /* We should never get here as control is now taken by the scheduler */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+void Start_Task(void *pvParameters){ 
+  //通过临界区确保业务创建成功
+  taskENTER_CRITICAL();
+  //创建事件组
+  eventGroup=xEventGroupCreate();
+  if(eventGroup==NULL){
+    printf("%s\n","event_group create fail");
+  }else{
+    printf("%s\n","event_group create success");
+   
+      xTaskCreate(Set_Task,"Set_Task",SET_STACK_SIZE,NULL,SET_PRIO,&set_Handler);
+      xTaskCreate(Wait_Task,"Wait_Task",WAIT_STACK_SIZE,NULL,WAIT_PRIO,&wait_Handler);
+      printf("all task create success\n");
+    }
+  vTaskDelete(NULL); //删除启动任务
+  taskEXIT_CRITICAL();//退出临界区
+}
+
+void Set_Task(void *pvParameters){
+  uint8_t keyNum=0;
+  while(1){
+    keyNum=Key_GetKeyNum();
+    if(keyNum==1){
+      //设置事件1置位
+      xEventGroupSetBits(eventGroup,KEY1_EVENT);
+      printf("key1 push down,event1 occur\n");
+    }else if(keyNum==2){
+      //设置事件2置位
+      xEventGroupSetBits(eventGroup,KEY2_EVENT);
+      printf("key2 push down,event2 occur\n");
+    }
+  osDelay(10);
+  }
+}
+
+void Wait_Task(void *pvParameters){
+  EventBits_t res=0;
+  while(1){
+    printf("core task waiting event occur\n");
+    res=xEventGroupWaitBits(eventGroup,KEY1_EVENT|KEY2_EVENT,pdTRUE,pdTRUE,5000);
+    //判断事件的发生情况
+    if((res&(KEY1_EVENT|KEY2_EVENT))==(KEY1_EVENT|KEY2_EVENT)){
+      printf("core task wait event occur success, core task running\n");
+    }else{
+      printf("core task wait event occur fail\n");
+
+    }
+    osDelay(1000);//进入阻塞态
+  }
+}
+
+/* USER CODE END 4 */
+
+/**
+  * @brief  Period elapsed callback in non blocking mode
+  * @note   This function is called  when TIM4 interrupt took place, inside
+  * HAL_TIM_IRQHandler(). It makes a direct call to HAL_IncTick() to increment
+  * a global variable "uwTick" used as application time base.
+  * @param  htim : TIM handle
+  * @retval None
+  */
+void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+{
+  /* USER CODE BEGIN Callback 0 */
+
+  /* USER CODE END Callback 0 */
+  if (htim->Instance == TIM4) {
+    HAL_IncTick();
+  }
+  /* USER CODE BEGIN Callback 1 */
+
+  /* USER CODE END Callback 1 */
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
